89ee805d4e88fe17790e5bd56ea35919d2e0f442
Nicolebalzano <nicole.balzano03@gmail.com>
2025-07-11 16:35:15 +0200

    header costumizzato

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -29,33 +29,20 @@ const linkNav = [
 
     return (
         <>
-            <header className="header-color">
-
-                {/* Navigazione */}
-                <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <Link className="text-white text-decoration-none fs-5 fw-bold mright-200" href="#">L8CD</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            {linkNav.map((curLink, index) => (
-                                <li className="nav-item" key={index}>
-                                    <Link to={curLink.url} className="nav-link text-white" aria-current="page" href="#">{curLink.title}</Link>
-                                </li>                               
-                            ))}
-                        </ul>
-                    </div>
-                    <form className="d-flex" role="search">
-                        <input className="search-control me-4" type="search" placeholder="Ricerca l'articolo..." aria-label=""/>
-                        <div className="py-2 me-3">
-                            <Link><i className="fa-solid fa-cart-shopping text-white"></i></Link>
-                        </div>
-                    </form>
+            <header className="header-color d-flex justify-content-between align-items-center">
+                <div className="header-logo">L8CD</div>
+                <div className="link-header">
+            <ul className="d-flex">
+                <li><Link>Offerte</Link></li>
+                <li><Link>Uomo</Link></li>
+                <li><Link>Donna</Link></li>
+                <li><Link>Bambino</Link></li>
+            </ul>
                 </div>
-                </nav>
-                <form className="d-flex d-none" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Ricerca l'articolo..." aria-label="Search"/>
-                </form>
+                
+                <div ><input type="text" placeholder="Cerca il tuo articolo... " className="search"/></div>
+                <div className="header-icons"><Link><i className="fa-solid fa-cart-shopping text-white"></i></Link></div>
+                
             </header>
         </>
     )
