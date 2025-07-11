329483084c794430997865124ea972d978f93f36
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-07-11 12:28:52 +0200

    feat: header style part 1 done, stylizing

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,49 +1,61 @@
 import { useState } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 
 const AppHeader = () => {
 
+
+
 // Link per la navigazione di pagina in pagina  
-const linkNav = [{
-
-    title: "Home",
-    url: "/"
-},
+const linkNav = [
 {
-
     title: "Offerte",
-    url: "/offerte"
+    url: "/offerte",
 },
 {
-
     title: "Uomo",
-    url: "/uomo"
+    url: "/uomo",
 },
 {
-
     title: "Donna",
-    url: "/donna"
+    url: "/donna",
 },
 {
-
     title: "Bambino",
-    url: "/bambino"
-}]
+    url: "/bambino",
+}
+]
+
+
 
     return (
         <>
-            <header>
-                <div className="container">
-                <nav className="navbar">
-                    <div className="container-fluid">
-                        <a className="navbar-brand">Navbar</a>
-                        <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn" type="submit">Search</button>
-                        </form>
+            <header className="header-color">
+
+                {/* Navigazione */}
+                <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <Link className="text-white text-decoration-none fs-5 fw-bold mright-200" href="#">L8CD</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {linkNav.map((curLink, index) => (
+                                <li className="nav-item" key={index}>
+                                    <Link to={curLink.url} className="nav-link text-white" aria-current="page" href="#">{curLink.title}</Link>
+                                </li>                               
+                            ))}
+                        </ul>
                     </div>
-                </nav>
+                    <form className="d-flex" role="search">
+                        <input className="search-control me-4" type="search" placeholder="Ricerca l'articolo..." aria-label=""/>
+                        <div className="py-2 me-3">
+                            <Link><i className="fa-solid fa-cart-shopping text-white"></i></Link>
+                        </div>
+                    </form>
                 </div>
+                </nav>
+                <form className="d-flex d-none" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Ricerca l'articolo..." aria-label="Search"/>
+                </form>
             </header>
         </>
     )
