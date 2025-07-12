59a8976f35aaf09e6f84926c094c89845c5fbb62
qsrdev <matteo.albertini747@gmail.com>
2025-07-12 10:44:41 +0200

    empty

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,58 +2,53 @@ import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
 const AppHeader = () => {
- 
-// Link per la navigazione di pagina in pagina  
-const linkNav = [
-{
-    title: "Novità",
-    url: "/shoes/novità",
-},
-{
-    title: "Uomo",
-    url: "/shoes/uomo",
-},
-{
-    title: "Donna",
-    url: "/shoes/donna",
-},
-{
-    title: "Bambino",
-    url: "/shoes/bambino",
-}
-]
+  // Link per la navigazione di pagina in pagina
+  const linkNav = [
+    {
+      title: "Novità",
+      url: "/shoes/novità",
+    },
+    {
+      title: "Uomo",
+      url: "/shoes/uomo",
+    },
+    {
+      title: "Donna",
+      url: "/shoes/donna",
+    },
+    {
+      title: "Bambino",
+      url: "/shoes/bambino",
+    },
+  ];
 
+  return (
+    <>
+      <header className="header-color d-flex justify-content-between align-items-center">
+        <div className="header-logo">L8CD</div>
 
-
-    return (
-        <>
-            <header className="header-color d-flex justify-content-between align-items-center">
-                <div className="header-logo">L8CD</div>
-                <div className="link-header">
-                    <ul className="d-flex">
-                        {linkNav.map((link) => (
-                            <li key={link.title}>
-                                <NavLink
-                                    to={link.url}
-                                    className={({ isActive }) =>
-                                    `text-white text-decoration-none ${isActive ? "fw-bold" : ""}`}>
-                                        {link.title}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                </div>               
-                <div ><input type="text" placeholder="Cerca il tuo articolo... " className="search"/></div>
-                <div className="header-icons"><Link><i className="fa-solid fa-cart-shopping text-white"></i></Link></div>
-                
-            </header>
-        </>
-    )
-}
-
-
-
-
+        <div className="link-header">
+          <ul className="d-flex">
+            {linkNav.map((link) => (
+              <li key={link.title}>
+                <NavLink to={link.url} className={({ isActive }) => `text-white text-decoration-none ${isActive ? "fw-bold" : ""}`}>
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <input type="text" placeholder="Cerca il tuo articolo... " className="search" />
+        </div>
+        <div className="header-icons">
+          <Link>
+            <i className="fa-solid fa-cart-shopping text-white"></i>
+          </Link>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default AppHeader;
-
