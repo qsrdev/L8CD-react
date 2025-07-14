3cc6acb59491703cc2221105e72f3525ba568286
qsrdev <matteo.albertini747@gmail.com>
2025-07-14 10:45:53 +0200

    feat: header fix

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,59 +4,60 @@ import BurgerMenu from "./BurgerMenu";
 import SearchInput from "./SearchInput";
 
 const AppHeader = () => {
- 
-// Link per la navigazione di pagina in pagina  
-const linkNav = [
-{
-    title: "Novità",
-    url: "/shoes/novita",
-},
-{
-    title: "Uomo",
-    url: "/shoes/uomo",
-},
-{
-    title: "Donna",
-    url: "/shoes/donna",
-},
-{
-    title: "Bambino",
-    url: "/shoes/bambino",
-}
-]
+  // Link per la navigazione di pagina in pagina
+  const linkNav = [
+    {
+      title: "Novità",
+      url: "/shoes/novita",
+    },
+    {
+      title: "Uomo",
+      url: "/shoes/uomo",
+    },
+    {
+      title: "Donna",
+      url: "/shoes/donna",
+    },
+    {
+      title: "Bambino",
+      url: "/shoes/bambino",
+    },
+  ];
 
+  return (
+    <>
+      <header className="header-color">
+        <div className="header-logo  me-auto">
+          <Link className="logo" to="/">
+            L8CD
+          </Link>
+        </div>
+        <div className="link-header">
+          <ul className="d-flex text-white">
+            {linkNav.map((curLink, index) => (
+              <li key={index}>
+                <Link to={curLink.url}>{curLink.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-
-    return (
-        <>
-            <header className="header-color">
-                <div className="header-logo  me-auto"><Link className="logo" to="/">L8CD</Link></div>
-                <div className="link-header">
-            <ul className="d-flex text-white">
-  {linkNav.map((curLink, index) => (
-    <li key={index}>
-      <Link to={curLink.url}>{curLink.title}</Link>
-    </li>
-  ))}
-</ul>
-                </div>
-    
-                
-                <div ><input type="text" placeholder="Cerca il tuo articolo... " className="search"/></div>
-            <div className="left-group d-flex align-items-center">
-                 <div>{<BurgerMenu/>}</div> 
-                <div className="header-icons"><Link><i className="fa-solid fa-cart-shopping text-white"></i></Link></div>
-             </div>
-            </header>
-                <div className="line"></div>
-    <SearchInput/>
-            
-        </>
-    )
-}
-
-
-
-
+        <div>
+          <input type="text" placeholder="Cerca il tuo articolo... " className="search" />
+        </div>
+        <div className="left-group d-flex align-items-center">
+          <div>{<BurgerMenu />}</div>
+          <div className="header-icons">
+            <Link>
+              <i className="fa-solid fa-cart-shopping text-white"></i>
+            </Link>
+          </div>
+        </div>
+      </header>
+      <div className="line"></div>
+      <SearchInput />
+    </>
+  );
+};
 
 export default AppHeader;
