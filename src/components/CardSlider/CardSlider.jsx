c709ad4c681296e99878c8bb35738a01c6f77722
Nicolebalzano <nicole.balzano03@gmail.com>
2025-07-14 12:54:05 +0200

    arrows aggiunte

--- src/components/CardSlider/CardSlider.jsx
+++ src/components/CardSlider/CardSlider.jsx
@@ -1,47 +1,46 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router-dom";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "./CardSlider.css";
 
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 
 export default function CardSlider({ array }) {
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 60,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {array.map((curElement, index) => (
-          <SwiperSlide key={index} className="slide">
-            <Link><img src={curElement.image} alt={curElement.name}/></Link>
-
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={10}
+      navigation 
+      pagination={{ clickable: true }}
+      breakpoints={{
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 60,
+        },
+      }}
+      modules={[Pagination, Navigation]} 
+      className="mySwiper"
+    >
+      {array.map((curElement, index) => (
+        <SwiperSlide key={index} className="slide">
+          <Link>
+            <img src={curElement.image} alt={curElement.name} />
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
