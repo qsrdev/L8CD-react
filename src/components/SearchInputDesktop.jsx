3b49fa73a5f87ebe67e71365266ae610482def97
samuele301197 <parisisamu32@gmail.com>
2025-07-15 17:06:17 +0200

    adjust: searchInputDesktop

--- src/components/SearchInputDesktop.jsx
+++ src/components/SearchInputDesktop.jsx
@@ -13,37 +13,39 @@ const SearchInputDesktop = () => {
   const handleRedirect = () => {
     if (search.trim()) {
       // Usa il valore inserito, non un brand fisso
-      navigate(`/shoes?brand=${encodeURIComponent(search.trim())}`);
+      navigate(`/shoes?q=${encodeURIComponent(search.trim())}`);
     }
   };
 
   return (
+    <div className="search-wrapper d-flex align-items-center gap-3">
+      <input
+        type="text"
+        placeholder="Cerca il tuo articolo..."
+        className="search d-sm-none d-md-none d-lg-block"
+        value={search}
+        onChange={handleInput}
+        onKeyDown={(e) => e.key === "Enter" && handleRedirect()}
+      />
 
-      <div className="search-wrapper d-flex align-items-center gap-3">
-          <input
-            type="text"
-            placeholder="Cerca il tuo articolo..."
-            className="search d-sm-none d-md-none d-lg-block"
-            value={search}
-            onChange={handleInput}
-            onKeyDown={(e) => e.key === "Enter" && handleRedirect()}
-          />
-
-          <button onClick={handleRedirect} className="btn-search-desktop d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
-          <div className="header-icons d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">
-            <Link to='/shoes/cart'>
-              <i className="fa-solid fa-cart-shopping text-white"></i>
-            </Link>
-          </div>
-
-        {search && (
-          <button className="clear-btn" onClick={() => setSearch("")}>
-            &#10005;
-          </button>
-        )}
+      <button
+        onClick={handleRedirect}
+        className="btn-search-desktop d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
+      >
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+      <div className="header-icons d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block">
+        <Link to="/shoes/cart">
+          <i className="fa-solid fa-cart-shopping text-white"></i>
+        </Link>
       </div>
+
+      {search && (
+        <button className="clear-btn" onClick={() => setSearch("")}>
+          &#10005;
+        </button>
+      )}
+    </div>
   );
 };
 
