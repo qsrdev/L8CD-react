bf4859e9750d025f03280c349c68b9a6c36669d6
Nicolebalzano <nicole.balzano03@gmail.com>
2025-07-16 16:31:46 +0200

    titolo levato

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useCart } from "../Context/CartContext";
 import { useState } from "react";
 import "../pages/Cart.css";
-import { useState } from "react";
+
 
 const Cart = () => {
   const {
