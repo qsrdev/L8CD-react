82240d356b1a8a56263a3f3f39c0dfe639d74a71
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-07-18 10:48:43 +0200

    feat: fixed burger menu link on click

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -28,24 +28,29 @@ const BurgerMenu = () => {
     },
   ];
 
+  const handleLinkClick = () => {
+    setMenuOpen(false)
+  }
+  
+
   return (
     <div className="burger-menu d-md-block d-lg-none">
       <div className="d-flex align-content-center">
-        <button className="btn" onClick={() => setMenuOpen(!menuOpen)}>
-          <i className="fs-5 fa-solid fa-bars text-white"></i>
-        </button>
         <div className="header-icon">
           <Link className="btn" to="/shoes/cart">
             <i className="fs-5 fa-solid fa-cart-shopping text-white"></i>
           </Link>
         </div>
+        <button className="btn" onClick={() => setMenuOpen(!menuOpen)}>
+          <i className="fs-5 fa-solid fa-bars text-white"></i>
+        </button>
       </div>
 
       {menuOpen && (
         <div className="container">
           <ul className="burger-nav py-3 position-absolute">
             {linkNav.map((link, index) => (
-              <li key={index} className="mb-2">
+              <li key={index} className="mb-2" onClick={handleLinkClick}>
                 <NavLink to={link.url}>
                   {link.title}
                 </NavLink>
