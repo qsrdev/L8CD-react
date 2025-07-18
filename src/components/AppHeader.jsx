76a1a644115892f52d3a1f9777e1fd959c51ced8
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-07-18 11:05:23 +0200

    feat: create search button for mobile

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -60,7 +60,6 @@ const AppHeader = () => {
           {<BurgerMenu />}
         </div>
       </header>
-      <SearchInput />
     </>
   );
 };
--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
 
 const BurgerMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [searchMobile, setSearchMobile] = useState(false)
 
   const linkNav = [
     {
@@ -32,18 +34,38 @@ const BurgerMenu = () => {
     setMenuOpen(false)
   }
   
+  const handleSearchClick = () => {
+    setSearchMobile(false)
+  }
+  
 
   return (
     <div className="burger-menu d-md-block d-lg-none">
+
+
       <div className="d-flex align-content-center">
+
+        {/* Link-cart */}
         <div className="header-icon">
           <Link className="btn" to="/shoes/cart">
             <i className="fs-5 fa-solid fa-cart-shopping text-white"></i>
           </Link>
         </div>
-        <button className="btn" onClick={() => setMenuOpen(!menuOpen)}>
-          <i className="fs-5 fa-solid fa-bars text-white"></i>
-        </button>
+
+        {/* Burger menu link */}
+        <div>
+          <button className="btn" onClick={() => setMenuOpen(!menuOpen)}>
+            <i className="fs-5 fa-solid fa-bars text-white"></i>
+          </button>
+        </div>
+
+        {/* Search Button */}
+        <div>
+          <button className="btn btn-search" onClick={() => setSearchMobile(!searchMobile)}>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+
       </div>
 
       {menuOpen && (
@@ -58,6 +80,10 @@ const BurgerMenu = () => {
             ))}
           </ul>
         </div>
+      )}
+
+      {searchMobile && (
+        <SearchInput onClick={handleSearchClick}/>
       )}
     </div>
   );
