cca1b96be01ba28f9053b464c418da4d0e0d912a
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-07-21 11:49:55 +0200

    feat: modify chatbot layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
           <section className="py-5">
             <div className="container g-4">
               <div className="mb-5">
-                <h2>Novità</h2>
+                <h2 className="fw-bold">Novità</h2>
                 <p>Scopri gli ultimi arrivi</p>
               </div>
               <CardSlider array={newShoes} />
@@ -55,7 +55,7 @@ const Home = () => {
           <section className="promo-color py-5">
             <div className="container g-4">
               <div className="mb-5 text-white">
-                <h2>Promo spedizione gratuita</h2>
+                <h2 className="fw-bold">Promo spedizione gratuita</h2>
                 <p>Approfitta ora della spedizione a costo zero</p>
               </div>
               <CardSlider array={freeShippingShoes} />
@@ -65,7 +65,7 @@ const Home = () => {
           <section className="py-5">
             <div className="container g-4">
               <div className="mb-5">
-                <h2>Nuovi brand!</h2>
+                <h2 className="fw-bold">Nuovi brand!</h2>
                 <p>Tutti gli ultimi arrivi</p>
                 <CardSlider array={randomShoes} />
               </div>
